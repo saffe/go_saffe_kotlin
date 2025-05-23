8f448c7c2ec76c2720092191c3d8cebfe4565bdb
Pedro Cruz <pedrovictor.lage@gmail.com>
2025-05-23 12:42:22 -0300

    Add extraData parameter to GoSaffeCapture

--- go_saffe_kotlin/src/main/java/ai/saffe/go_saffe_kotlin/GoSaffeCapture.kt
+++ go_saffe_kotlin/src/main/java/ai/saffe/go_saffe_kotlin/GoSaffeCapture.kt
@@ -23,12 +23,29 @@ import androidx.core.content.ContextCompat
 import com.scottyab.rootbeer.RootBeer
 import org.json.JSONObject
 
+interface Settings {
+    val primaryColor: String?
+    val secondaryColor: String?
+    val lang: String?
+}
+
+interface SendResultsTo {
+    val media: String
+    val email: String
+}
+
+interface ExtraData {
+    val settings: Settings?
+    val sendResultsTo: SendResultsTo?
+}
+
 class GoSaffeCapture(
     private val context: Context,
     private val captureKey: String?,
     private val user: String?,
     private val type: String?,
     private val endToEndId: String?,
+    private val extraData: ExtraData? = null,
     private val onClose: (() -> Unit)? = null,
     private val onFinish: (() -> Unit)? = null,
     private val onTimeout: (() -> Unit)? = null,
@@ -180,11 +197,28 @@ class GoSaffeCapture(
         WebView.setWebContentsDebuggingEnabled(true)
 
         val jsonBody = JSONObject().apply {
-            put("capture_key", captureKey ?: JSONObject.NULL)
-            put("user_identifier", user ?: JSONObject.NULL)
-            put("type", type ?: JSONObject.NULL)
-            put("end_to_end_id", endToEndId ?: JSONObject.NULL)
+            captureKey?.let { put("capture_key", it) }
+            user?.let { put("user_identifier", it) }
+            type?.let { put("type", it) }
+            endToEndId?.let { put("end_to_end_id", it) }
             put("device_context", getDeviceContext())
+            extraData?.let {
+                put("extra_data", JSONObject().apply {
+                    it.settings?.let { settings ->
+                        put("settings", JSONObject().apply {
+                            settings.primaryColor?.let { put("primary_color", it) }
+                            settings.secondaryColor?.let { put("secondary_color", it) }
+                            settings.lang?.let { put("lang", it) }
+                        })
+                    }
+                    it.sendResultsTo?.let { sendResultsTo ->
+                        put("send_results_to", JSONObject().apply {
+                            put("media", sendResultsTo.media)
+                            put("email", sendResultsTo.email)
+                        })
+                    }
+                })
+            }
         }
         val postData = jsonBody.toString().toByteArray(Charsets.UTF_8)
 
